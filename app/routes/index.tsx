2db78b7201381da8d221bcb5bed9795c3f745098
Steve Steinitz <steve@stzdev.com>
2025-02-15 15:46:29 +1100

    Not exhibiting import export issue

---
+++ app/routes/index.tsx
@@ -0,0 +1,26 @@
+// app/routes/index.tsx
+import { createFileRoute, useRouter } from '@tanstack/react-router'
+import {getCount, updateCount } from '../lib/count'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => await getCount(),
+})
+
+function Home() {
+  const router = useRouter()
+  const state = Route.useLoaderData()
+
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        updateCount({ data: 1 }).then(() => {
+          router.invalidate()
+        })
+      }}
+    >
+      Add 1 to {state}?
+    </button>
+  )
+}

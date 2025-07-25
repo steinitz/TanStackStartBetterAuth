aaab27cf6bf8c590a5978f1b57e9000f71e3fcde
Steve Steinitz <steve@stzdev.com>
2025-07-25 17:25:28 +1000

    Restore __root.tsx file for virtual routing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -45,12 +45,6 @@ export const Route = createRootRoute({
     ]
   }),
   component: RootComponent,
-  notFoundComponent: () => (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-    </div>
-  ),
 })
 
 function RootComponent() {
